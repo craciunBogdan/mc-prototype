19c9dcf89ee23ca647e197a024b674a113eb246c
Dorinel Panaite <van.panaite@gmail.com>
2020-05-31 18:14:46 +0200

    Add parameters to webapp.js

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Read the README.md in `packages/webapp` for more info.
 
 You can start the app from the root folder by running:
 
-- `./webapp.js` (hopefully also runs on windows) - this starts the app in non-HTTPS (for now). 
+- `./webapp.js` (hopefully also runs on windows) - this starts the app in non-HTTPS. 
+    - Optional parameters:
+        - `--install` (performs `npm install` before running).
+        - `--https` (runs the app in https mode)
 
 Don't forget to `cd` into that directory first when running other commands.
--- webapp.js
+++ webapp.js
@@ -3,7 +3,17 @@ const spawnSync = require('child_process').spawnSync;
 
 const npm = process.platform === 'win32' ? 'npm.cmd' : 'npm';
 
-spawnSync(npm, ['start'], {
+// Perform 'npm install'
+if (process.argv.includes('--install')) {
+    spawnSync(npm, ['install'], {
+        cwd: './packages/webapp',
+        stdio: 'inherit'
+    });
+}
+
+// Decide if starting with https or not
+const startCommand = process.argv.includes('--https') ? 'start:https' : 'start';
+spawnSync(npm, ['run', startCommand], {
     cwd: './packages/webapp',
     stdio: 'inherit'
 });
