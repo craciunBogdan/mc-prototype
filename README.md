6c2e1de0ba7263af3e3531981c4c3f868f9e6a1f
Dorinel Panaite <van.panaite@gmail.com>
2020-06-01 12:31:16 +0200

    Upgrade webapp.js
    - it install node_modules for the first time when ran, so '--install' can be used as a force install now
    - maybe it will work on windows with https

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Read the README.md in `packages/webapp` for more info.
 
 You can start the app from the root folder by running:
 
-- `./webapp.js` (hopefully also runs on windows) - this starts the app in non-HTTPS. 
+- `node webapp.js` (hopefully also runs on windows) - this starts the app in non-HTTPS and installs packages if `node_modules` folder is missing. 
     - Optional parameters:
-        - `--install` (performs `npm install` before running).
+        - `--install` (force `npm install` before running).
         - `--https` (runs the app in https mode)
 
 Don't forget to `cd` into that directory first when running other commands.
--- webapp.js
+++ webapp.js
@@ -1,19 +1,33 @@
-#!/usr/bin/env node
 const spawnSync = require('child_process').spawnSync;
-
+const fs = require('fs');
+const path = require('path');
 const npm = process.platform === 'win32' ? 'npm.cmd' : 'npm';
 
-// Perform 'npm install'
-if (process.argv.includes('--install')) {
+const webappPath = path.join('packages', 'webapp');
+
+console.log('WHAT');
+
+// Force perform 'npm install'
+if (!fs.existsSync(path.join(webappPath, 'node_modules')) || process.argv.includes('--install')) {
+    console.log('RUNNING npm install...');
     spawnSync(npm, ['install'], {
-        cwd: './packages/webapp',
+        cwd: webappPath,
         stdio: 'inherit'
     });
 }
 
 // Decide if starting with https or not
-const startCommand = process.argv.includes('--https') ? 'start:https' : 'start';
-spawnSync(npm, ['run', startCommand], {
-    cwd: './packages/webapp',
-    stdio: 'inherit'
-});
+if (process.argv.includes('--https')) {
+    const startCommand = process.platform === 'win32' ? '($env:HTTPS = "true") -and (npm start)' : 'npm run start:https';
+
+    spawnSync(startCommand, {
+        cwd: webappPath,
+        stdio: 'inherit',
+        shell:true
+    });
+} else {
+    spawnSync(npm, ['start'], {
+        cwd: webappPath,
+        stdio: 'inherit'
+    });
+}
