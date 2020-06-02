8734d91b02e7f6e00f7623a52d703a7deda2e561
Dorinel Panaite <van.panaite@gmail.com>
2020-06-02 23:14:18 +0200

    Send color from home screen

--- packages/webapp/src/components/home/Home.js
+++ packages/webapp/src/components/home/Home.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 import { CirclePicker } from 'react-color';
 import './Home.css';
+import AudioTransmitter from '../common/audio-transmitter';
 
 function Home() {    
     const colorArray = ['#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548', '#607d8b'];
     const [color, setColor] = useState('#22194D');
+    const [audioTransmitter, setAudioTransmitter] = useState(new AudioTransmitter());
 
     const handleChange = (color) => {
-        setColor(color.hex)
+        setColor(color.hex);
     };
 
     const handleChangeComplete = (color) => {
-        setColor(color.hex)
+        setColor(color.hex);
+
+        // Update the color in the audio transmitter
+        audioTransmitter.updatePlaybackValue(color.hex);
+
+        // Refresh audio transmitter
+        setAudioTransmitter(audioTransmitter);
     }
 
     const sendColor = () => {
+        audioTransmitter.startPlaying();
         // TODO
         console.log(`Sent ${color}`);
     }
