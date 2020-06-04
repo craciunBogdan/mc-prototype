7bcdec062f262caaeed54a97dba65a8efc00919e
Dorinel Panaite <van.panaite@gmail.com>
2020-06-04 19:00:42 +0200

    Use self updater in color picker

--- packages/webapp/src/components/home/components/color-picker/ColorPicker.js
+++ packages/webapp/src/components/home/components/color-picker/ColorPicker.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CirclePicker } from 'react-color';
 import './ColorPicker.css';
 import '../../../common/components/styles/containers.css';
@@ -9,7 +9,13 @@ function ColorPicker() {
     const colorArray = ['#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548', '#607d8b'];
     const [color, setColor] = useState('#22194D');
     const [audioTransmitter, setAudioTransmitter] = useState(new AudioTransmitter());
+    const [buttonText, setButtonText] = useState('Send color');
 
+    if (audioTransmitter.selfStateUpdater !== setAudioTransmitter) {
+        audioTransmitter.selfStateUpdater = setAudioTransmitter;
+        setAudioTransmitter(audioTransmitter);
+    }
+    
     const handleChange = (color) => {
         setColor(color.hex);
     };
@@ -19,24 +25,16 @@ function ColorPicker() {
 
         // Update the color in the audio transmitter
         audioTransmitter.updatePlaybackValue(color.hex);
-
-        // Refresh audio transmitter
-        setAudioTransmitter(audioTransmitter);
     }
 
     const sendColor = () => {
         if (audioTransmitter.isPlayingBack) {
-            return;
+            audioTransmitter.stopPlaying();
+            setButtonText('Send color');
+        } else {
+            audioTransmitter.startPlaying();
+            setButtonText('Stop sending');
         }
-
-        audioTransmitter.startPlaying((at) => {
-            setAudioTransmitter(at);
-        });
-
-        // Refresh audio transmitter
-        setAudioTransmitter(audioTransmitter);
-
-        console.log(`Sent ${color}`);
     }
 
     return (
@@ -54,8 +52,7 @@ function ColorPicker() {
             <div className="bottom-button-container">
                 <RoundedButton
                     color={color}
-                    disabled={audioTransmitter.isPlayingBack}
-                    text={'Send color'}
+                    text={buttonText}
                     onClick={sendColor}
                 />
             </div>
