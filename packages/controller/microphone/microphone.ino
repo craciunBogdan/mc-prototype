21bb3c7cf2cfdf48696b6528a9481d8338f7fa79
Diana <soponar.diana@gmail.com>
2020-06-08 21:57:10 +0200

    Added parts of the encryption

--- packages/controller/microphone/microphone.ino
+++ packages/controller/microphone/microphone.ino
@@ -1,5 +1,7 @@
 #include "arduinoFFT.h"
- 
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <ChaCha.h> // Encryption Library
+
 #define SAMPLES 1024             //Must be a power of 2
 #define SAMPLING_FREQUENCY 10000 //Hz, must be less than 10000 due to ADC
 
@@ -12,11 +14,14 @@
 
 arduinoFFT FFT = arduinoFFT();
 
-//TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
+TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 
 int redpin = 25; // select the pin for the red LED
 int greenpin = 26;// select the pin for the green LED
 int bluepin = 27; // select the pin for the blue LED
+int freq = 2000;
+int channel = 0;
+int resolution = 8;
  
 unsigned int sampling_period_us;
 unsigned long microseconds;
@@ -28,19 +33,22 @@ int arrayIndex;
 boolean recording;
 double recordedValues[MAX_LENGTH];
 long timeOfRecording;
+
+String key_string = "****************";
+String input_string = "Helloo"; 
+byte key_bytes[32];
+uint8_t counter[8] = {109, 110, 111, 112, 113, 114, 115, 116};
+uint8_t output[17], input[17], output_decrypt[17];
+uint8_t nonce[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
+ChaCha chacha = ChaCha();
  
 void setup() {
+  tft.init();
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE);
+  
   Serial.begin(115200);
-
-  ledcAttachPin(redpin, 1);
-  ledcAttachPin(bluepin, 2);
-  ledcAttachPin(greenpin, 3);
-
-  ledcSetup(1, 12000, 8);
-  ledcSetup(2, 12000, 8);
-  ledcSetup(3, 12000, 8);
  
-  // Serial.println(sampling_period_us);
   sampling_period_us = round(1000000*(1.0/SAMPLING_FREQUENCY));
 
   // microphone
@@ -49,10 +57,58 @@ void setup() {
   pinMode(35, INPUT);
   arrayIndex = 0;
   timeOfRecording = 0;
+  // beeper
+  ledcSetup(channel, freq, resolution);
+  ledcAttachPin(17, channel);
+
+  key_string.getBytes(key_bytes, 17);
+  Serial.println(key_string);
+  for (int i=0; i < 17; i++){
+    Serial.println(key_bytes[i]);
+  }
+  chacha.setKey(key_bytes, 16);
+  chacha.setIV(nonce, 12);
+//  chacha.setCounter(counter, 8);
+  chacha.setNumRounds(20);
+
+  input_string.getBytes(input, 17);
+  Serial.println("Input");
+  for (int i=0; i < 17; i++){
+    Serial.println(input[i]);
+  }
+  chacha.encrypt(output, input, 5);
+  Serial.println("Encryption");
+  for (int i=0; i < 17; i++){
+    Serial.println(output[i]);
+  }
+
+  chacha.setKey(key_bytes, 16);
+  chacha.setIV(nonce, 12);
+  chacha.setNumRounds(20);
+//  chacha.setCounter(counter, 8);
+  
+  chacha.decrypt(output_decrypt, output, 17);
+  Serial.println("Decryption");
+  for (int i=0; i < 17; i++){
+    Serial.println(output_decrypt[i]);
+  }
 }
  
 void loop() {
   if (digitalRead(35) == LOW) {
+    // first time recording send request
+    if (recording == false) {
+      ledcWrite(channel, 150);
+      ledcWriteTone(channel, 1739);
+      delay(1000);
+//      requestColor();
+      requestInteger();
+//      requestString();
+      delay(1000);
+      ledcWriteTone(channel, 1739);
+      delay(1000);
+      ledcWrite(channel, 0);
+    }
     Serial.println("Button down ---> Recording");
     recording = true;
     // just started to record
@@ -86,22 +142,32 @@ void loop() {
     delay(250);  //Repeat the process every second OR:
     // while(1);       //Run code once
   } else {
-      // when the button was released
-      if (recording == true){
-        Serial.println("Button up");
-        recording = false;
-//        for(int i = 0; i < MAX_LENGTH; i++) {
-//          if (recordedValues[i] != 0) {
-//            Serial.println(recordedValues[i]);
-//          }
-//        }
-        // process recorded values
-        processRecording();
-        //
-        //
-        // clean recorded values
+    // when the button was released
+    if (recording == true){
+      Serial.println("Button up");
+      recording = false;
+      // process recorded values
+      processRecording();
+      // clean recorded values
+      for(int i = 0; i < MAX_LENGTH; i++) {
+        if (recordedValues[i] != 0) {
+          recordedValues[i] = 0;
+        }
       }
+    }
   }
+}
+
+void requestColor() {
+  ledcWriteTone(channel, 2087);
+}
+
+void requestInteger() {
+  ledcWriteTone(channel, 3305);
+}
+
+void requestString() {
+  ledcWriteTone(channel, 4697);
 }
 
 void processRecording() {
@@ -111,13 +177,11 @@ void processRecording() {
   double counter = 1;
   double realSampleRate = 1000/250;
   for(int i = 0; i < arrayIndex - 1; i++) {
-//    Serial.println(recordedValues[i]);
-//    Serial.println(recordedValues[i + 1]);
     if (!areSameFrequency(recordedValues[i], recordedValues[i + 1])){
       recordingMap[index] = recordedValues[i];
       sampleRate[index] = counter / realSampleRate;
 //      Serial.println(recordingMap[index]);
-      Serial.println(sampleRate[index]);
+//      Serial.println(sampleRate[index]);
       counter = 1;
       index++;
     } else {
@@ -147,12 +211,15 @@ void processData (double recordingMap[], double sampleRate[], int arraySize) {
       }
     }
   }
-  processColor(dataValues, dataIndex);
+//  processColor(dataValues, dataIndex);
+  processInteger(dataValues, dataIndex);
+//  processString(dataValues, dataIndex);
 }
 
 void processColor (double dataValues[], int arraySize) {
   double chunkedColor[arraySize/2];
   int indexChunk = 0;
+  tft.fillScreen(TFT_BLACK);
   if (arraySize != 6) {
     Serial.println("Unexpected number of values were received");
   } else {
@@ -161,11 +228,55 @@ void processColor (double dataValues[], int arraySize) {
       Serial.println(chunkedColor[indexChunk]);
       indexChunk++;
     }
-    // GBR - for some reason
-    ledcWrite(1, chunkedColor[1]);
-    ledcWrite(2, chunkedColor[2]);
-    ledcWrite(3, chunkedColor[0]);
+    // LED
+    ledcAttachPin(redpin, 1);
+    ledcAttachPin(bluepin, 2);
+    ledcAttachPin(greenpin, 3);
+  
+    ledcSetup(1, 12000, 8);
+    ledcSetup(2, 12000, 8);
+    ledcSetup(3, 12000, 8);
+    
+    ledcWrite(1, chunkedColor[0]);
+    ledcWrite(2, chunkedColor[1]);
+    ledcWrite(3, chunkedColor[2]);
+    tft.drawString("Color received", 5, 90, 2);
+    tft.drawNumber(chunkedColor[0], 5, 120, 2);
+    tft.drawNumber(chunkedColor[0], 25, 120, 2);
+    tft.drawNumber(chunkedColor[0], 45, 120, 2);
+
   }
+}
+
+void processInteger (double dataValues[], int arraySize) {
+  unsigned int value = 0;
+  tft.fillScreen(TFT_BLACK);
+  if (arraySize != 8) {
+    Serial.println("Unexpected number of values were received");
+  } else {
+    for (int i = arraySize - 1; i >= 0; i--) {
+      value = (value * 16) + dataValues[i];
+    }
+    Serial.println(value);
+    tft.drawString("Integer received", 5, 90, 2);
+    tft.drawNumber(value, 5, 120, 4);
+  }
+}
+
+void processString (double dataValues[], int arraySize) {
+  double chunkedString[arraySize/2];
+  int indexChunk = 0;
+  String values = "";
+  tft.drawString(values, 5, 120, 4);
+  tft.fillScreen(TFT_BLACK);
+  tft.drawString("String received", 5, 90, 2);
+  for (int i = 0; i < arraySize; i += 2) {
+    chunkedString[indexChunk] = (dataValues[i+1] * 16) + dataValues[i];
+    values.concat(char(chunkedString[indexChunk]));
+    Serial.println(chunkedString[indexChunk]);
+    indexChunk++;
+  }
+  tft.drawString(values, 5, 120, 4);
 }
 
 int frequencyToByte(double freq) {
